40b7e89e5f013254bcd7dd31240b957887c38c1e
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-05-08 11:25:59 -0300

    feat: add 45.-Jump-Game-II

---
+++ 45.-Jump-Game-II/2/index.ts
@@ -0,0 +1,32 @@
+function jump(nums: number[]): number {
+  const lastIndex = nums.length - 1
+  let currentIndex = 0
+  let jumpTimes = 0
+
+  if(nums.length === 1) return 0
+
+  while(true) {
+    const currentJump = nums[currentIndex]
+
+    if (currentJump >= lastIndex - currentIndex) {
+      return jumpTimes + 1
+    }
+
+    let nextMaxJump = 0
+    let nextMaxJumpIndex = 0
+    for (let i = currentIndex + 1; i <= currentJump + currentIndex; i++) {
+      // calculate the next best jump
+      const jumpPower = nums[i] + i
+      if (nextMaxJump < jumpPower) {
+        nextMaxJump = jumpPower
+        nextMaxJumpIndex = i
+      }
+    }
+
+    jumpTimes++
+
+    currentIndex = nextMaxJumpIndex
+  }
+};
+
+console.log(jump([4,1,1,3,1,1,1]))
